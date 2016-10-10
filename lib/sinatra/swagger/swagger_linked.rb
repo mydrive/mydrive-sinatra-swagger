32217ae372d20ddd123789b5f3cf71c8b1aa6eea
Matthew <matthew.hinks@mydrivesolutions.com>
2016-10-10 13:40:02 +0100

    Added in strict enforcement for request paths, verbs and response schemas

--- lib/sinatra/swagger/swagger_linked.rb
+++ lib/sinatra/swagger/swagger_linked.rb
@@ -12,6 +12,14 @@ module Sinatra
       end
 
       module Helpers
+        def request_path
+          env['REQUEST_PATH'] || env['PATH_INFO']
+        end
+
+        def request_verb
+          env['REQUEST_METHOD'].downcase
+        end
+
         def swagger_spec
           raise "No swagger file loaded" unless settings.swagger
           settings.swagger.request_spec(env: env)
@@ -21,7 +29,7 @@ module Sinatra
           schema = swagger_spec[:spec]
           path.split("/").each do |key|
             schema = schema[YAML.load(key)]
-            return if schema.nil?
+            raise "No schema response matching path: #{path} for #{request_verb} #{request_path}" if schema.nil?
           end
           schema['definitions'] = settings.swagger['definitions'] if settings.swagger['definitions']
           schema
